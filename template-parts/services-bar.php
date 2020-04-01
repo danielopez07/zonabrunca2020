f1343f3830e40057e04b1d54169e72076bc84988
Daniel Lopez <danielopez07@gmail.com>
2020-03-31 23:13:33 -0300

    Translating homepage links to services / businesses

--- template-parts/services-bar.php
+++ template-parts/services-bar.php
@@ -2,21 +2,21 @@
   <h2><?php _e('Servicios', 'zonabrunca2020'); ?></h2>
   <p>Descripción de los servicios</p>
   <ul>
-    <li>
+    <a href="<?php _e('/cosas-que-hacer', 'zonabrunca2020'); ?>"><li>
       <i class="fa fa-binoculars" aria-hidden="true"></i>
       <span><?php _e('Cosas que hacer', 'zonabrunca2020'); ?></span>
-    </li>
-    <li>
+    </li></a>
+    <a href="<?php _e('/donde-comer', 'zonabrunca2020'); ?>"><li>
       <i class="fa fa-cutlery" aria-hidden="true"></i>
       <span><?php _e('Donde comer', 'zonabrunca2020'); ?></span>
-    </li>
-    <li>
+    </li></a>
+    <a href="<?php _e('/donde-alojarse', 'zonabrunca2020'); ?>"><li>
       <i class="fa fa-bed" aria-hidden="true"></i>
       <span><?php _e('Donde alojarse', 'zonabrunca2020'); ?></span>
-    </li>
-    <li>
+    </li></a>
+    <a href="<?php _e('/servicios-adicinales', 'zonabrunca2020'); ?>"><li>
       <i class="fa fa-shopping-bag" aria-hidden="true"></i>
       <span><?php _e('Servicios adicinales', 'zonabrunca2020'); ?></span>
-    </li>
+    </li></a>
   </ul>
 </nav>
