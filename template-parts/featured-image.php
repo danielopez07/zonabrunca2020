ad7cc0ec040b4eae87005819730f22231ce675cf
Daniel Lopez <danielopez07@gmail.com>
2020-05-09 23:37:55 -0300

    Fix size thumbnail search

--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -12,9 +12,8 @@ if ( has_post_thumbnail() && ! post_password_required() ) {
 	$featured_media_inner_classes = '';
 
 	// Make the featured media thinner on archive pages.
-	if ( ! is_singular() || ! is_search() ) {
-		$featured_media_inner_classes .= ' medium';
-	} elseif (is_search()) {
+	if ( ! is_singular() ) {
+		// $featured_media_inner_classes .= ' medium';
 		$featured_media_inner_classes .= ' small';
 	}
 	?>
