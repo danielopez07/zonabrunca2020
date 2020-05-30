119a188576834a59c573616cb78bb388a59be8a4
Daniel Lopez <danielopez07@gmail.com>
2020-05-30 01:14:38 -0300

    slick responsiveness, featured businesses section

--- template-parts/featured-businesses.php
+++ template-parts/featured-businesses.php
@@ -49,16 +49,16 @@
             slidesToShow: 3,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 2000,
+            autoplaySpeed: 3000,
             speed: 300,
             dots: false,
             centerMode: true,
-            focusOnSelect: true,
+            // focusOnSelect: true,
             responsive: [
                 {
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 3,
+                        slidesToShow: 2,
                         slidesToScroll: 1,
                         infinite: true,
                     }
@@ -66,15 +66,16 @@
                 {
                     breakpoint: 600,
                         settings: {
-                            slidesToShow: 2,
+                            slidesToShow: 1,
                             slidesToScroll: 1
                         }
                 },
                 {
-                    breakpoint: 480,
+                    breakpoint: 500,
                         settings: {
                             slidesToShow: 1,
-                            slidesToScroll: 1
+                            slidesToScroll: 1,
+                            centerMode: false,
                         }
                 }
                 // You can unslick at a given breakpoint now by adding:
