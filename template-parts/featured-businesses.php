31acb2869f4197ff05f15782c0e50ae8cc58ba3d
Daniel Lopez <danielopez07@gmail.com>
2020-05-30 00:41:58 -0300

    Addinig categories to featured businesses section

--- template-parts/featured-businesses.php
+++ template-parts/featured-businesses.php
@@ -1,5 +1,5 @@
 <section class="featured-businesses">
-    <!-- <h2><?php _e( 'Destacados', 'zonabrunca2020' ) ?></h2> -->
+    <h2><?php _e( 'Destacados', 'zonabrunca2020' ) ?></h2>
 
     <div class="slider-nav">
         <?php
@@ -46,11 +46,41 @@
     <script>
     jQuery(document).ready(function() {
         jQuery('.slider-nav').slick({
-            slidesToShow: 2,
+            slidesToShow: 3,
             slidesToScroll: 1,
-            dots: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            speed: 300,
+            dots: false,
             centerMode: true,
-            focusOnSelect: true
+            focusOnSelect: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1
+                        }
+                },
+                {
+                    breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
         });
     })
     </script>
