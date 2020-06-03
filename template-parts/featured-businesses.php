caddd5675f74de2e53dbe2820604067249d68d80
Daniel Lopez <danielopez07@gmail.com>
2020-06-03 18:15:06 -0300

    Corrections to slick

--- template-parts/featured-businesses.php
+++ template-parts/featured-businesses.php
@@ -60,11 +60,17 @@
             focusOnSelect: true,
             responsive: [
                 {
+                    breakpoint: 1600,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
                     breakpoint: 1024,
                     settings: {
                         slidesToShow: 2,
                         slidesToScroll: 1,
-                        infinite: true,
                     }
                 },
                 {
@@ -73,6 +79,7 @@
                             slidesToShow: 1,
                             slidesToScroll: 1,
                             centerMode: false,
+                            arrows: true
                         }
                 },
                 // You can unslick at a given breakpoint now by adding:
