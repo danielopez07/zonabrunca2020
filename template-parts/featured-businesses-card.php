c0a65cb45c7b1aee864f383dff8d89db6fdf60de
Daniel Lopez <danielopez07@gmail.com>
2020-06-05 19:42:02 -0300

    Componentizing featured businesses cards

---
+++ template-parts/featured-businesses-card.php
@@ -0,0 +1,27 @@
+<div><article class="card">
+    <a href="' . esc_url( get_permalink() ) . '">
+    <?php
+        $img = get_the_post_thumbnail($the_query->post->ID, 'medium');
+        if($img)
+            echo '<figure class="card-image">' . get_the_post_thumbnail($the_query->post->ID, 'medium') . '</figure>';
+        else {
+            echo '<figure class="card-image"><img src="/wp-content/themes/zonabrunca2020/imgs/BallenaUvita-mobile.jpg"></figure>';
+        }
+    ?>
+    </a>
+
+    <div class="content-card">
+        <?php
+        echo '<a href="' . esc_url( get_permalink() ) . '">';
+        echo '<h3>' . get_the_title() . '</h3>';
+        echo '</a>';
+        ?>
+        <div class="categories-card">
+            <span class="screen-reader-text"><?php _e( 'Categories', 'twentytwenty' ); ?></span>
+            <?php the_category( ', ' ); ?>
+        </div>
+        <div class="excerpt-card">
+            <?php the_excerpt(); ?>
+        </div>
+    </div>
+</article></div>
--- template-parts/featured-businesses.php
+++ template-parts/featured-businesses.php
@@ -32,34 +32,9 @@
         <?php 
         while ( $the_query->have_posts() ) : 
             $the_query->the_post();
-            
-            echo '<div><article class="card">';
-                echo '<a href="' . esc_url( get_permalink() ) . '">';
-                    $img = get_the_post_thumbnail($the_query->post->ID, 'medium');
-                    if($img)
-                        echo '<figure class="card-image">' . get_the_post_thumbnail($the_query->post->ID, 'medium') . '</figure>';
-                    else {
-                        echo '<figure class="card-image"><img src="/wp-content/themes/zonabrunca2020/imgs/BallenaUvita-mobile.jpg"></figure>';
-                    }
-                echo '</a>';
 
-                ?>
-                <div class="content-card">
-                    <?php
-                    echo '<a href="' . esc_url( get_permalink() ) . '">';
-                    echo '<h3>' . get_the_title() . '</h3>';
-                    echo '</a>';
-                    ?>
-                    <div class="categories-card">
-                        <span class="screen-reader-text"><?php _e( 'Categories', 'twentytwenty' ); ?></span>
-                        <?php the_category( ', ' ); ?>
-                    </div>
-                    <div class="excerpt-card">
-                        <?php the_excerpt(); ?>
-                    </div>
-                </div>
-                <?php
-            echo '</article></div>';
+            set_query_var( 'the_query', $the_query );
+            get_template_part( 'template-parts/featured-businesses-card' );
         endwhile; 
         wp_reset_postdata(); 
         ?>
