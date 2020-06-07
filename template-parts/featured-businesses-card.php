77f34704381f17578d4cde0ec7b52877cc22945b
Daniel Lopez <danielopez07@gmail.com>
2020-06-07 16:23:10 -0300

    link in card excerpt

--- template-parts/featured-businesses-card.php
+++ template-parts/featured-businesses-card.php
@@ -1,5 +1,5 @@
 <div><article class="card">
-    <a href="<?php esc_url( get_permalink() ) ?> '">
+    <a href="<?php echo esc_url( get_permalink() ) ?> ">
     <?php
         $img = get_the_post_thumbnail($the_query->post->ID, 'medium');
         if($img)
@@ -11,17 +11,17 @@
     </a>
 
     <div class="content-card">
-        <?php
-        echo '<a href="' . esc_url( get_permalink() ) . '">';
-        echo '<h3>' . get_the_title() . '</h3>';
-        echo '</a>';
-        ?>
+        <a href="<?php echo esc_url( get_permalink() ) ?> ">
+            <h3><?php the_title() ?></h3>
+        </a>
         <div class="categories-card">
             <span class="screen-reader-text"><?php _e( 'Categories', 'twentytwenty' ); ?></span>
             <?php the_category( ', ' ); ?>
         </div>
-        <div class="excerpt-card">
-            <?php the_excerpt(); ?>
-        </div>
+        <a href="<?php echo esc_url( get_permalink() ) ?> ">
+            <div class="excerpt-card">
+                <?php the_excerpt(); ?>
+            </div>
+        </a>
     </div>
 </article></div>
