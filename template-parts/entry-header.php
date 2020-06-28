54355280af45966fefd13add4f01ce5e8e1ff7d7
Daniel Lopez <danielopez07@gmail.com>
2020-06-28 18:48:57 -0300

    taking out excerpt from posts

--- template-parts/entry-header.php
+++ template-parts/entry-header.php
@@ -47,29 +47,6 @@ if ( is_singular() ) {
 		} else {
 			the_title( '<h2 class="entry-title heading-size-1"><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
 		}
-
-		$intro_text_width = '';
-
-		if ( is_singular() ) {
-			$intro_text_width = ' small';
-		} else {
-			$intro_text_width = ' thin';
-		}
-
-		if ( has_excerpt() && is_singular() ) {
-			?>
-
-			<div class="intro-text section-inner max-percentage<?php echo $intro_text_width; //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?>">
-				<?php the_excerpt(); ?>
-			</div>
-
-			<?php
-		}
-
-		// Default to displaying the post meta.
-		// if ( ! is_search() ) {
-		// 	twentytwenty_the_post_meta( get_the_ID(), 'single-top' );
-		// }
 		?>
 
 	</div><!-- .entry-header-inner -->
